311b1763e1379538ff10f33637f7f8c8b0c7f9ef
AAAAAAAq <747590770@qq.com>
2018-07-08 23:37:49 +0800

    登录页面完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>loginn</div>
+  <div class="login-wrap">
+    <el-form label-position="top" label-width="80px" :model="formDate" class="login-form">
+      <h2>用户登录</h2>
+  <el-form-item label="用户名">
+    <el-input v-model="formData.username"></el-input>
+  </el-form-item>
+  <el-form-item label="活动区域">
+    <el-input v-model="formData.password"></el-input>
+  </el-form-item>
+ <el-button type="primary" class="login-btn">登录</el-button>
+</el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style>
+  .login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.login-wrap .login-form {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.login-wrap .login-form .login-btn {
+  width: 100%;
+}
 </style>
