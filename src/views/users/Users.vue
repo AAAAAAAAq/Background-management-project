e43e2c108f11a0646424c23729751fde317f83bc
AAAAAAAq <747590770@qq.com>
2018-07-09 16:41:31 +0800

    获取并渲染用户信息

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -2,21 +2,90 @@
   <el-card class="box-card">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <!-- 面包屑 -->
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-    <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-</el-breadcrumb>
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索区域 -->
+    <el-row class="seachArea">
+      <el-col :span="24">
+        <el-input clearable placeholder="请输入内容" class="searchInput">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+     <el-table
+      stripe
+      border
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+      type="index"
+      width="50">
+    </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+      <el-table-column
+        prop="mg_state"
+        label="用户状态">
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list:[]
+    }
+  },
+  created () {
+    this.loadData()
+  },
+  methods: {
+    async loadData () {
+      // 发送请求前获取token
+      const token = sessionStorage.getItem('token')
+      // 在请求头中设置token
+      this.$http.defaults.headers.common['Authorization'] = token
+      const res = await this.$http.get('users?pagenum=1&pagesize=10')
+      const data = res.data
+      const {meta: { msg, status }} = data
+      if(status ===200){
+        const {data:{users}} = data 
+        this.list = users
+      }else{
+        this.$message.error(msg)
+      }
+    }
+  }
 }
 </script>
 
 <style>
   .box-card {
     height: 100%;
+  }
+  .seachArea {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .seachArea .searchInput{
+    width: 350px;
   }
 </style>
