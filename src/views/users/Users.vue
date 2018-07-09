3af57b3bf778159f976e108b645c68b8b25b38a2
AAAAAAAq <747590770@qq.com>
2018-07-09 15:35:26 +0800

    面包屑和卡片布局

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>用户列表</div>
+  <el-card class="box-card">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <!-- 面包屑 -->
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+    <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+</el-breadcrumb>
+  </el-card>
 </template>
 
 <script>
@@ -9,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+  .box-card {
+    height: 100%;
+  }
 </style>
