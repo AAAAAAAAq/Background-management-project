def55bea06a49413161ad9de35168800e31e36d1
AAAAAAAq <747590770@qq.com>
2018-07-10 21:46:58 +0800

    权限列表路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from '../views/login.vue'
 import Home from '../views/home.vue'
 import Users from '../views/users/Users.vue'
+import Rights from '../views/roles/Rights.vue'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
           name: 'users',
           path: '/users',
           component: Users
+        },
+        {
+          name: 'rights',
+          path: '/rights',
+          component: Rights
         }
       ]
     }
