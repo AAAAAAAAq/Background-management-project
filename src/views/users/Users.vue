4c0b4b673c1b382438c277ba3bf3de0929583b00
AAAAAAAq <747590770@qq.com>
2018-07-10 21:26:36 +0800

    分配用户角色完成

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -62,24 +62,24 @@
         <template slot-scope="scope">
           <el-button type="primary" icon="el-icon-edit" size="mini" plain @click="handleSerPer(scope.row)"></el-button>
           <el-button type="danger" icon="el-icon-delete" size="mini" plain @click="handleDel(scope.row.id)"></el-button>
-          <el-button type="success" icon="el-icon-check" size="mini" plain></el-button>
+          <el-button type="success" icon="el-icon-check" size="mini" plain @click="handlePerRights(scope.row)"></el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 添加用户的弹出框 -->
     <el-dialog title="添加用户" :visible.sync="AddFormVisible"  @closed="handleClear">
-      <el-form v-model="AddFromData" label-width="100px" :rules="rules" ref="AddFromData">
+      <el-form v-model="FromData" label-width="100px" :rules="rules" ref="FromData">
         <el-form-item label="姓名"  prop="username">
-          <el-input v-model="AddFromData.username" auto-complete="off"></el-input>
+          <el-input v-model="FromData.username" auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input v-model="AddFromData.password" auto-complete="off" type="password"></el-input>
+          <el-input v-model="FromData.password" auto-complete="off" type="password"></el-input>
         </el-form-item>
         <el-form-item label="邮箱" prop="email">
-          <el-input v-model="AddFromData.email" auto-complete="off"></el-input>
+          <el-input v-model="FromData.email" auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item label="电话" prop="mobile">
-          <el-input v-model="AddFromData.mobile" auto-complete="off"></el-input>
+          <el-input v-model="FromData.mobile" auto-complete="off"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -89,20 +89,43 @@
     </el-dialog>
     <!-- 编辑用户的弹出框 -->
     <el-dialog title="修改用户" :visible.sync="EditFormVisible"  @closed="handleClear">
-      <el-form v-model="AddFromData" label-width="100px" :rules="rules" ref="AddFromData">
+      <el-form v-model="FromData" label-width="100px" :rules="rules" ref="FromData">
         <el-form-item label="姓名" readonly>
-          <el-input v-model="AddFromData.username" auto-complete="off" :disabled="true"></el-input>
+          <el-input v-model="FromData.username" auto-complete="off" :disabled="true"></el-input>
         </el-form-item>
         <el-form-item label="邮箱" prop="email">
-          <el-input v-model="AddFromData.email" auto-complete="off"></el-input>
+          <el-input v-model="FromData.email" auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item label="电话" prop="mobile">
-          <el-input v-model="AddFromData.mobile" auto-complete="off"></el-input>
+          <el-input v-model="FromData.mobile" auto-complete="off"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="EditFormVisible = false" >取 消</el-button>
         <el-button type="primary" @click="handleEdit">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 角色分配弹出框 -->
+    <el-dialog title="角色分配" :visible.sync="RightFormVisible">
+      <el-form>
+        <el-form-item label="用户名">
+         {{ currentUserName }}
+        </el-form-item>
+        <el-form-item label="角色">
+          <el-select  v-model="currentRoleID">
+            <el-option label="请选择" disabled :value="-1"></el-option>
+            <el-option
+              v-for="item in roles"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="RightFormVisible= false">取 消</el-button>
+        <el-button type="primary" @click="handelSetRoles">确 定</el-button>
       </div>
     </el-dialog>
       <!-- 分页 -->
@@ -122,24 +145,33 @@
 export default {
   data () {
     return {
+      // 后台返回数据
       list: [],
+      // 加载提示
       loading: true,
+      // 分页数据
       pagenum: 1,
       pagesize: 5,
       total: 0,
       searchVal: '',
-      AddFromData: {
+      // 绑定表单数据
+      FromData: {
         username: '',
         password: '',
         email: '',
         mobile: ''
       },
       editID: -1,
+      // 控制显示添加用户弹出框
       AddFormVisible: false,
+      // 控制显示编辑用户弹出框
       EditFormVisible: false,
+      // 控制显示角色分配弹出框
+      RightFormVisible: false,
+      // 表单验证的规则
       rules: {
         username: [
-          { required: true, message: '请输入姓名', trigger: 'blur' },
+          { required: true, message: '请输入姓名', trigger: 'blur' }, // 触发的条件 失去焦点
           { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
         ],
         password: [
@@ -153,7 +185,12 @@ export default {
           { type: 'number', message: '请输入正确的电话', trigger: 'blur' },
           { min: 8, max: 11, message: '请输入正确的电话', trigger: 'blur' }
         ]
-      }
+      },
+      // 分配角色需要数据
+      currentUserName: '',
+      currentRoleID: -1,
+      currentUserID: -1,
+      roles: []
     }
   },
   created () {
@@ -231,13 +268,13 @@ export default {
     // 添加用户
     async handleAdd () {
       this.AddFormVisible = false
-      const res = await this.$http.post('users', this.AddFromData)
+      const res = await this.$http.post('users', this.FromData)
       const data = res.data
       const {meta: { status, msg }} = data
       if (status === 201) {
         this.$message.success(msg)
         this.loadData()
-        handleClear()
+        this.handleClear()
       } else {
         this.$message.error(msg)
       }
@@ -256,32 +293,63 @@ export default {
     resetForm (formName) {
       this.$refs[formName].resetFields()
     },
-    // 获取编辑用户信息
+    // 获取编辑用户信息显示到弹出框
     async handleSerPer (user) {
       this.EditFormVisible = true
       this.editID = user.id
       const res = await this.$http.get(`users/${this.editID}`)
-      this.AddFromData = res.data.data
-
+      this.FromData = res.data.data
     },
     // 编辑用户
-    async handleEdit(user){
-      const res = await this.$http.put(`users/${this.editID}`,this.AddFromData)
+    async handleEdit () {
+      const res = await this.$http.put(`users/${this.editID}`, this.FromData)
       const data = res.data
-      const {meta:{ msg, status }} = data 
-      if( status === 200){
+      const {meta: { msg, status }} = data
+      if (status === 200) {
         this.EditFormVisible = false
         this.$message.success(msg)
         this.loadData()
-      }else{
+      } else {
         this.$message.error(msg)
       }
     },
     // 清除文本框
-    handleClear(){
-      for (const key in this.AddFromData) {
-          this.AddFromData[key] = ''
-        }
+    handleClear () {
+      for (const key in this.FromData) {
+        this.FromData[key] = ''
+      }
+    },
+    // 显示分配角色的弹出框信息
+    async handlePerRights (user) {
+      this.currentUserName = user.username
+      this.currentUserID = user.id
+      // 弹出框显示
+      this.RightFormVisible = true
+      // 获取所有角色信息
+      const res = await this.$http.get('roles')
+      this.roles = res.data.data
+      // 根据用户id查询用户信息：角色id
+      const res1 = await this.$http.get(`users/${user.id}`)
+      this.currentRoleID = res1.data.data.rid
+    },
+    async handelSetRoles (user) {
+      const res = await this.$http.put(`users/${this.currentUserID}/role`, {
+        rid: this.currentRoleID
+      })
+      const data = res.data
+      const { meta: { msg, status } } = data
+      if (status === 200) {
+        // 关闭弹出框
+        this.RightFormVisible = false
+        // 提示
+        this.$message.success(msg)
+        // 重置数据
+        this.currentUserName = ''
+        this.currentRoleID = -1
+        this.currentUserID = -1
+      } else {
+        this.$message.error(msg)
+      }
     }
   }
 }
