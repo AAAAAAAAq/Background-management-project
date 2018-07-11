d8dcd42242fc361c43ce2d02c64cd0ab8e923fdf
AAAAAAAq <747590770@qq.com>
2018-07-11 10:34:37 +0800

    权限管理-角色列表渲染数据

--- src/views/roles/Roles.vue
+++ src/views/roles/Roles.vue
@@ -15,6 +15,13 @@
       :data="list"
       style="width: 100%">
       <el-table-column
+        type="expand"
+        width="50">
+        <template>
+  
+        </template>
+      </el-table-column>
+      <el-table-column
         type="index"
         width="50">
       </el-table-column>
@@ -24,13 +31,17 @@
         width="180">
       </el-table-column>
       <el-table-column
-        prop="role"
+        prop="roleDesc"
         label="角色描述"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="level"
         label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" plain ></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" plain ></el-button>
+          <el-button type="success" icon="el-icon-check" size="mini" plain ></el-button>
+        </template>
       </el-table-column>
     </el-table>
   </el-card>
@@ -38,7 +49,28 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.loadData()
+  },
+  methods: {
+    // 加载角色列表
+    async loadData(){
+      // resData是服务器返回的数据 
+      const { data: resData } = await this.$http.get('roles')
+      // 解析 data status msg
+      const { data, meta: { status, msg }} = resData
+      if (status === 200) {
+        this.list = data
+      }else{
+        this.$message.error(msg)
+      }
+    }
+  }
 }
 </script>
 
