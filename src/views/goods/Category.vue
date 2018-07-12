d492d15a0895d1d28fbb4422b0b957d392756f82
AAAAAAAq <747590770@qq.com>
2018-07-12 19:06:18 +0800

    商品分类-搭建页面结构

--- src/views/goods/Category.vue
+++ src/views/goods/Category.vue
@@ -1,13 +1,60 @@
 <template>
-  <div>商品列表</div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <my-breadcrumb level1="商品管理" level2="商品分类"></my-breadcrumb>
+    <!-- 按钮 -->
+    <el-row>
+      <el-col :span="24" class="btn">
+        <el-button>添加分类</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <el-table
+      border
+      stripe
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="cat_name"
+        label="分类名称">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="级别">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="是否有效">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" plain ></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" plain ></el-button>
+          <el-button type="success" icon="el-icon-check" size="mini" plain ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
 <style>
-
+  .btn {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 </style>
